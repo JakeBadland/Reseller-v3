a96adc7b6912d1b5d09f66be7b868229a79ffe8a
Jake Badland <jake.badland@gmail.com>
2023-03-30 13:03:08 +0300

    IDE Git test

--- app/Views/layouts/main.php
+++ app/Views/layouts/main.php
@@ -6,7 +6,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
-    <title>Reseller v3.0.7</title>
+    <title>Reseller v3.0.8</title>
 
     <script type="text/javascript" src="/js/jquery-3.2.1.min.js"></script>
     <script type="text/javascript" src="/js/bootstrap.min.js"></script>
