cf10c61e113b17cd99c91ec9ef1035081bc6d63a
Jake Badland <jake.badland@gmail.com>
2023-07-12 16:25:08 +0300

    game moving grid test

--- public/js/game.js
+++ public/js/game.js
@@ -4,15 +4,23 @@ let game = {
         lat: 0.0,
         lon: 0.0
     },
+    gridStep : 38.5,
+    canvasHeight : null,
+    canvasWidth : null,
+    visibleRadius : 500,
 
     init : function (){
+
+        if ($('canvas').length == 0){
+            return;
+        }
+
         this.addEvents();
-        this.drawMap();
         this.getAnomalies();
 
         let options = {
-            enableHighAccuracy: false,
-            timeout: 1000,
+            enableHighAccuracy: true,
+            timeout: 5000,
             maximumAge: 0,
         };
 
@@ -33,19 +41,24 @@ let game = {
         });
     },
     drawAnomalies : function (){
+        //console.log(game.anomalies.length);
         let distance = 0;
         let near = 999999999;
-        for (let i=0; i<game.anomalies.length; i++){
+        let name = '';
+        for (let i = 0; i < game.anomalies.length; i++){
             distance = game.calculateDistance(
                 game.location.lat,
                 game.location.lon,
                 game.anomalies[i].lat,
                 game.anomalies[i].lon,
             );
-            if (distance < near){ near = distance; }
+            if (distance < near){
+                near = distance;
+                name = game.anomalies[i].name;
+            }
         }
 
-        $('#anoma_near').text(near + 'm');
+        $('#anoma_near').text(near + 'm (' + name + ')');
     },
     showPosition : function (position){
         game.location.lat = position.coords.latitude;
@@ -58,12 +71,14 @@ let game = {
 
         $.post( "/game/save-user-loc", {data : game.location});
 
+        game.drawMap();
         game.drawAnomalies();
     },
     drawMap : function(){
         let canvas = document.querySelector('canvas')
         let ctx = canvas.getContext('2d')
-        let step = 50;
+
+        ctx.clearRect(0, 0, canvas.width, canvas.height);
 
         let cW = $('#map').parent('.map-col').width();
 
@@ -72,22 +87,34 @@ let game = {
 
         ctx.canvas.height = ctx.canvas.width = cW;
 
-        this.drawGrid(ctx, cW, step);
+        this.drawGrid(ctx, cW, game.gridStep);
     },
     drawGrid :  function(ctx, cW, step){
+        let offset = game.calculateOffset();
+        game.calculateOffset();
         ctx.strokeStyle = "white";
         ctx.lineWidth = 0.3;
-        for (let i = 1; (i * step) < cW; i++){
-            ctx.moveTo(i * step, 0);
-            ctx.lineTo(i * step, cW);
 
-            ctx.moveTo(0, i * step);
-            ctx.lineTo(cW, i * step);
+        for (let i = 1; (i * step) < cW; i++){
+            ctx.moveTo(i * step + offset.X, 0);
+            ctx.lineTo(i * step + offset.X, cW);
+
+            ctx.moveTo(0, i * step + offset.Y);
+            ctx.lineTo(cW, i * step + offset.Y);
         }
         ctx.arc(cW / 2, cW / 2, cW / 2, 0, Math.PI*2, true);
         ctx.stroke();
     },
-    calculateDistance : function (lat1, lon1, lat2, lon2) {
+    calculateOffset : function (){
+        let diffLat = game.calculateDistance(0, 0, 46.42865, 0, true);
+        let diffLon = game.calculateDistance(0, 0, 0, 30.72420, true);
+
+        return {
+            X : Math.round(diffLon / 100 % 38.5),
+            Y : Math.round(diffLat / 100 % 38.5)
+        };
+    },
+    calculateDistance : function (lat1, lon1, lat2, lon2, highAccuracy = false) {
         const earthRadius = 6371; // Радиус Земли в километрах
 
         // Преобразуем градусы в радианы
@@ -105,6 +132,11 @@ let game = {
             Math.sin(dLat / 2) ** 2 +
             Math.cos(lat1Rad) * Math.cos(lat2Rad) * Math.sin(dLon / 2) ** 2;
         const c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
+
+        if (highAccuracy){
+            return earthRadius * c * 1000;
+        }
+
         return Math.round(earthRadius * c * 1000);
     },
 
